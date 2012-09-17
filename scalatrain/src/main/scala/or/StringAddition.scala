e3da0eda217ffb45fde01521f7c87b11631a242d
Magnus Larsson <ml@glodit.se>
2012-09-17 21:56:14 +0200

    First version of minus (not working yet)

--- scalatrain/src/main/scala/or/StringAddition.scala
+++ scalatrain/src/main/scala/or/StringAddition.scala
@@ -1,12 +1,30 @@
 package or
 
 class StringAddition(thisString: String) {
-  val OVERFLOW_INDICATOR = "<"
-  def stringPlus(i: String): String = "2"
-  def overflow(s: String) = s.startsWith(OVERFLOW_INDICATOR)
-  def toInt(c: Char) = (c + "").toInt
+  private val OVERFLOW_INDICATOR = "<"
+  private def overflow(s: String) = s.startsWith(OVERFLOW_INDICATOR)
+  private def toInt(c: Char) = (c + "").toInt
+  private def tion(thatString: String, f: (String, Tuple2[Char, Char]) => String) = {
+    thisString.reverse.zipAll(thatString.reverse, '0', '0').foldLeft("") { f(_, _) }
+  }
+  def minus(thatString: String) = {
+    val subtrac = (s: String, n: Tuple2[Char, Char]) => {
+      val o = if (overflow(s)) s.substring(2, 3).toInt else 0
+      println("o" + o)
+      val newS = if (overflow(s)) s.substring(3) else s
+      println("newS" + newS)
+      val sum = toInt(n._1) - toInt(n._2) - o
+      println("sum" + sum)
+      if (sum >= 0)
+        sum + newS
+      else
+        OVERFLOW_INDICATOR + sum + newS
+    }
+    val result = tion(thatString, subtrac)
+    if (overflow(result)) result.substring(1) else result
+  }
   def plus(thatString: String) = {
-    val result = thisString.reverse.zipAll(thatString.reverse, '0', '0').foldLeft("") { (s, n) =>
+    val result = thisString.view.reverse.zipAll(thatString.view.reverse, '0', '0').foldLeft("") { (s, n) =>
       {
         val o = if (overflow(s)) s.substring(1, 2).toInt else 0
         val newS = if (overflow(s)) s.substring(2) else s
--- scalatrain/src/test/scala/or/StringAdditionTest.scala
+++ scalatrain/src/test/scala/or/StringAdditionTest.scala
@@ -5,12 +5,17 @@ import org.specs2.mutable._
 class StringAdditionTest extends Specification {
   implicit def stringAddition(s: String) = new StringAddition(s)
   "Strings" should {
-    "support addition" in {
+    "support " in {
       "5" plus "6" must equalTo("11")
-    }
-    "support addition" in {
       "99999999999999999999999999999999999999999999999999999999999999999999999999999999999999" plus "1" must equalTo(
         "100000000000000000000000000000000000000000000000000000000000000000000000000000000000000")
+    }
+    "support minus" in {
+      //      "1" minus "1" must equalTo("0")
+      //      "2" minus "1" must equalTo("1")
+      //      "1" minus "2" must equalTo("-1")
+      "10" minus "1" must equalTo("9")
+      //"100000000000000000000000000000000000000000000000000000000000000000000000000000000000000" minus "99999999999999999999999999999999999999999999999999999999999999999999999999999999999999" must equalTo("1")
     }
   }
 }
