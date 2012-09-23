c7612458140bb8a570ba498d29f9a1e8e01fe92c
Magnus Larsson <maguaking@gmail.com>
2012-09-23 21:03:16 +0200

    Euler 18 and start of euler67

--- euler/src/test/scala/or/EulerTest017.scala
+++ euler/src/test/scala/or/EulerTest017.scala
@@ -10,24 +10,27 @@ import scala.annotation.tailrec
 import java.util.concurrent.TimeUnit
 
 class EulerTest017 extends Specification {
-  
 
-  
+
+
   "If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total" should {
-  val m = Map(1 -> "one", 2 -> "two", 3 -> "three", 4 -> "four", 5 -> "five",
+
+    val m = Map(1 -> "one", 2 -> "two", 3 -> "three", 4 -> "four", 5 -> "five",
     6 -> "six", 7 -> "seven", 8 -> "eight", 9 -> "nine", 10 -> "ten",
     11 -> "eleven", 12 -> "twelve", 13 -> "thirteen", 14 -> "fourteen", 15 -> "fifteen",
     16 -> "sixteen", 17 -> "seventeen", 18 -> "eighteen", 19 -> "nineteen")
-  val t = Map(20 -> "twentie", 30 -> "thirtie", 40 -> "fourty", 50 -> "fifty",
+    val t = Map(10 -> "ten", 20 -> "twentie", 30 -> "thirtie", 40 -> "fourty", 50 -> "fifty",
     60 -> "sixtie", 70 -> "seventie", 80 -> "eighty", 90 -> "ninety")
   val hundred = "hundred"
   def numberToString(i: Int): String = {
     if (1 <= i && i <= 19) m(i)
     else "zero"
   }
+
     "If all the numbers from 1 to 1000 (one thousand) inclusive were written out" in {
       numberToString(1) must equalTo("one")
       numberToString(10) must equalTo("ten")
+      numberToString(20) must equalTo("twenty")
     }
   }
 }
---
+++ euler/src/test/scala/or/EulerTest018.scala
@@ -0,0 +1,62 @@
+package or
+
+import org.specs2.mutable.Specification
+import scala.collection.mutable.Map
+import scala.collection.mutable.HashMap
+import scala.annotation.tailrec
+
+class EulerTest018 extends Specification {
+
+
+
+  "If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total" should {
+val smallTriangle = """3
+7 4
+2 4 6
+8 5 9 3"""
+val bigTriangle = """75
+95 64
+17 47 82
+18 35 87 10
+20 04 82 47 65
+19 01 23 75 03 34
+88 02 77 73 07 63 67
+99 65 04 28 06 16 70 92
+41 41 26 56 83 40 80 70 33
+41 48 72 33 47 32 37 16 94 29
+53 71 44 65 25 43 91 52 97 51 14
+70 11 33 28 77 73 17 78 39 68 17 57
+91 71 52 38 17 14 91 43 58 50 27 29 48
+63 66 04 68 89 53 67 30 73 16 69 87 40 31
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23"""
+lazy val hugeTriangle = {
+  val source = io.Source.fromURL(getClass.getResource("/euler67triangle.txt"))
+  //val lines = source .mkString
+  val ll = source.getLines.toList
+  source.close
+  ll
+}
+
+  def maximumValuePath(triangleS: String): Int = {
+    maximumValuePathList(triangleS.split("\\n").toList)
+  }
+  def maximumValuePathList(triangle: List[String]): Int = {
+    val ll = triangle.map(_.split(" ").toList.map(_.toInt))
+    val s = ll.size
+    def rec(row: Int, col: Int): Int = {
+      val thisValue = ll(row)(col)
+      if (row + 1 < s)
+        thisValue + (rec(row + 1, col) max rec(row + 1, col + 1))
+      else
+        thisValue
+    }
+    rec(0, 0)
+  }
+
+    "If all the numbers from 1 to 1000 (one thousand) inclusive were written out" in {
+      maximumValuePath(smallTriangle) must equalTo(23)
+      maximumValuePath(bigTriangle) must equalTo(1074)
+      //maximumValuePathList(hugeTriangle) must equalTo(1074)
+    }
+  }
+}
