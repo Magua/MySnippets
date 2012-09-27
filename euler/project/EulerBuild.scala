406160628877734f60ed83907887e27cee6015af
Magnus Larsson <ml@glodit.se>
2012-09-27 20:26:20 +0200

    Scalariform

--- euler/project/EulerBuild.scala
+++ euler/project/EulerBuild.scala
@@ -6,7 +6,9 @@ object EulerBuild extends Build {
   lazy val euler = Project(
     id = "euler",
     base = file("."),
-    settings = Project.defaultSettings ++ Seq(
+    settings = Project.defaultSettings ++
+      scalariformSettings ++
+      Seq(
       name := "euler",
       organization := "or",
       version := "0.1-SNAPSHOT",
--- euler/project/plugins.sbt
+++ euler/project/plugins.sbt
@@ -1,3 +1,5 @@
 
 addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "2.1.0")
 
+addSbtPlugin("com.typesafe.sbtscalariform" % "sbtscalariform" % "0.5.1")
+
